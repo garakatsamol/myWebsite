c33673ab6360ec40f3cd0d4c09dac12769f26fd5
garakatsamol <tasosioan@gmail.com>
2023-09-24 23:32:59 +0300

    3 files modified

--- src/components/SingleFairy.vue
+++ src/components/SingleFairy.vue
@@ -1,45 +1,45 @@
 <script setup lang="ts">
-
 import { ref, onMounted } from 'vue';
-import { useTresContext, useRaycaster } from '@tresjs/core'
-import {
-  Sphere,
-  useAnimations,
-} from '@tresjs/cientos';
+import { useTresContext, useRaycaster } from '@tresjs/core';
+import { Sphere, useAnimations } from '@tresjs/cientos';
 import { clone } from 'three/examples/jsm/utils/SkeletonUtils.js';
 
-
-
-const context = useTresContext()
-const { fairyNodes, fairyScene, fairyAnimations, fairyPosition } = defineProps(['fairyNodes', 'fairyScene', 'fairyAnimations', 'fairyPosition'])
+const context = useTresContext();
+const { fairyNodes, fairyScene, fairyAnimations, fairyPosition } = defineProps([
+  'fairyNodes',
+  'fairyScene',
+  'fairyAnimations',
+  'fairyPosition',
+]);
 
 const fairy = clone(fairyNodes['RootNode']);
-const fairyRef = ref(null)
+const fairyRef = ref(null);
 
-const sphereRef = ref(null)
-const emit = defineEmits(['fairyClicked'])
-
+const sphereRef = ref(null);
+const emit = defineEmits(['fairyClicked']);
 
 onMounted(() => {
-
- sphereRef.value.value.material.visible=false;
-
+  sphereRef.value.value.material.visible = false;
 });
-const fairyAnim = fairyAnimations[0]
-const { actions, mixer } = useAnimations([fairyAnim], fairy)
-let currentAction = actions.idle
-currentAction.play()
-mixer.timeScale = Math.random()
+const fairyAnim = fairyAnimations[0];
+const { actions, mixer } = useAnimations([fairyAnim], fairy);
+let currentAction = actions.idle;
+currentAction.play();
+mixer.timeScale = Math.random();
 const onClick = (intersection, pointerEvent) => {
-const fairyInter = intersection.object
- 
-  emit('fairyClicked', fairyPosition);
+  const fairyInter = intersection.object;
 
-}
+  emit('fairyClicked', fairyPosition);
+};
 </script>
 
 <template>
-  <Sphere @click="onClick" ref="sphereRef" :position="fairyPosition" :args="[1, 10, 10]">
+  <Sphere
+    @click="onClick"
+    ref="sphereRef"
+    :position="fairyPosition"
+    :args="[1, 10, 10]"
+  >
     <primitive ref="fairyRef" :object="fairy" :scale="[0.2, 0.2, 0.2]" />
   </Sphere>
 </template>
--- src/components/TheModel.vue
+++ src/components/TheModel.vue
@@ -1,31 +1,29 @@
 <script setup lang="ts">
 import { Ref, onMounted, ref, watch, watchEffect } from 'vue';
-import { TresCanvas, useRenderLoop, useTexture, } from '@tresjs/core';
-import { reactive, shallowRef, } from 'vue';
+import { TresCanvas, useRenderLoop, useTexture } from '@tresjs/core';
+import { reactive, shallowRef } from 'vue';
 //import Fairy from './Fairy.vue';
 import SingleFairy from './SingleFairy.vue';
 import {
   OrbitControls,
   MouseParallax,
-
   useTweakPane,
   useGLTF,
   useAnimations,
   Stars,
-  Smoke
+  Smoke,
 } from '@tresjs/cientos';
-import gsap from 'gsap'
+import gsap from 'gsap';
 const state = reactive({
   clearColor: '#201919',
   shadows: true,
   alpha: false,
-
 });
-const cameraRef = shallowRef<PerspectiveCamera>()
+const cameraRef = shallowRef<PerspectiveCamera>();
 const orbitRef = ref(null);
-let menuClicked = false
+let menuClicked = false;
 const cameraPosition = { x: 0, y: 0, z: -0 };
-const cameraIdlePosition = [0, 0, 10]
+const cameraIdlePosition = [0, 0, 10];
 watchEffect(() => {
   if (cameraRef.value) {
     const position = cameraRef.value.position;
@@ -44,22 +42,16 @@ const {
 });
 
 const moveCamera = (fairyPosition: Vector3) => {
+  let newCameraPosition = menuClicked ? cameraIdlePosition : fairyPosition;
+  let cameraOffset = menuClicked ? 0 : 4;
 
-  let newCameraPosition = menuClicked ? cameraIdlePosition : fairyPosition
-  let cameraOffset = menuClicked ? 0 : 4
-
-  gsap.to(cameraRef.value.position,
-    {
-      x: newCameraPosition[0],
-      y: newCameraPosition[1],
-      z: newCameraPosition[2] + cameraOffset,
-
-    }
-
-  )
-  menuClicked = !menuClicked
-}
-
+  gsap.to(cameraRef.value.position, {
+    x: newCameraPosition[0],
+    y: newCameraPosition[1],
+    z: newCameraPosition[2] + cameraOffset,
+  });
+  menuClicked = !menuClicked;
+};
 </script>
 
 <template>
@@ -72,25 +64,41 @@ const moveCamera = (fairyPosition: Vector3) => {
     <TresGroup :position="[0, 0, 0]">
       <Stars />
       <Suspense>
-        <SingleFairy :fairyNodes="nodes" :fairyScene="knight" :fairyAnimations="animations" :fairyPosition="[0, 3, 1]"
-          @fairyClicked=moveCamera />
-
-
+        <SingleFairy
+          :fairyNodes="nodes"
+          :fairyScene="knight"
+          :fairyAnimations="animations"
+          :fairyPosition="[0, 3, 1]"
+          @fairyClicked="moveCamera"
+        />
       </Suspense>
       <Suspense>
-        <SingleFairy @fairyClicked=moveCamera :fairyNodes="nodes" :fairyScene="knight" :fairyAnimations="animations"
-          :fairyPosition="[-2, -3, -0]" />
-
+        <SingleFairy
+          @fairyClicked="moveCamera"
+          :fairyNodes="nodes"
+          :fairyScene="knight"
+          :fairyAnimations="animations"
+          :fairyPosition="[-2, -3, -0]"
+        />
       </Suspense>
       <Suspense>
-        <SingleFairy @fairyClicked=moveCamera :fairyNodes="nodes" :fairyScene="knight" :fairyAnimations="animations"
-          :fairyPosition="[2, 1, -1]" />
+        <SingleFairy
+          @fairyClicked="moveCamera"
+          :fairyNodes="nodes"
+          :fairyScene="knight"
+          :fairyAnimations="animations"
+          :fairyPosition="[2, 1, -1]"
+        />
       </Suspense>
-
     </TresGroup>
-    <TresAmbientLight :intensity=".1" :color="0xffffff" />
+    <TresAmbientLight :intensity="0.1" :color="0xffffff" />
 
-    <TresDirectionalLight :position="[0, 1, 10]" :intensity="0.1" :color="0xffffff" cast-shadow />
+    <TresDirectionalLight
+      :position="[0, 1, 10]"
+      :intensity="0.1"
+      :color="0xffffff"
+      cast-shadow
+    />
     <TresAxesHelper />
     <TresGridHelper :args="[10, 10, 0x444444, 'teal']" />
   </TresCanvas>
